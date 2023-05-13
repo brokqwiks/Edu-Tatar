63dbc71b8aa454b96eba9ee6bbd727c0fd2eafcf
brokqwiks <brokqwiks@mail.ru>
2023-05-13 22:51:24 +0300

    'release'

--- bot/script_bot.py
+++ bot/script_bot.py
@@ -8,6 +8,7 @@ from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButt
 import config
 import sys
 
+
 bot = Bot(config.bot_token)
 dp = Dispatcher(bot, 
                 storage=MemoryStorage())
@@ -16,6 +17,7 @@ class ClientStatesGroup(StatesGroup):
     LoginEduTatar = State()
     DiaryEduTatar = State()
 
+#Создаем клавиатуры
 StartKeyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 LoginEduTatar_Button = KeyboardButton('Войти')
 StartKeyboard.add(LoginEduTatar_Button)
@@ -25,43 +27,62 @@ DiaryEduTatar_Button = KeyboardButton('Дневник')
 CancelEduTatar_Button = KeyboardButton('Выйти')
 LoginKeyboard.add(DiaryEduTatar_Button).insert(CancelEduTatar_Button)
 
-
+#Команда start
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     await bot.send_message(message.from_user.id, 'Привет! Этот бот - электронный дневник EduTatar.\nЧтобы посмотреть, что умеет этот бот войди в свой аккаунт',
                            reply_markup=StartKeyboard)
 
+#Команада Войти
 @dp.message_handler(Text(equals='Войти'))
 async def cmd_sing_in(message: types.Message):
     await bot.send_message(message.from_user.id, 'Отправь свой Логин и пароль в таком формате:\n\n"Логин"\n"Пароль"')
     await ClientStatesGroup.LoginEduTatar.set()
 
-@dp.message_handler(state=ClientStatesGroup.LoginEduTatar)
-async def LoginEduTatar(message: types.Message, state: FSMContext):
-    LoginFromMessage = message.text.split('\n')[0]
-    PasswordFromMessge = message.text.split('\n')[1]
-    
-    sys.path.insert(0, 'C:/Users/user/MyProjects/MyPythonProjects/Edu Tatar/script')
-    import script_login
-
-    LoginEduTatar = script_login.loginEduTatar(LoginFromMessage, PasswordFromMessge)
-    
-    if LoginEduTatar[0] == 'true':
-        await bot.send_message(message.from_user.id, f'Успешная авторизация!\nЛогин: {LoginEduTatar[1][1]}\nФИО: {LoginEduTatar[1][0]}',
-                               reply_markup=LoginEduTatar)
-        await state.finish()
-        await ClientStatesGroup.DiaryEduTatar.set()
-
-        @dp.message_handler(state=ClientStatesGroup.DiaryEduTatar)
-        async def cmd_diary(message: types.Message, state: FSMContext):
-            if message.text == 'Дневник':
-                DiaryEduTatar = script_login.diary(LoginFromMessage, PasswordFromMessge)
-                print(DiaryEduTatar[0])
-                await state.finish()
-            elif message.text == 'Выйти':
-                await bot.send_message(message.from_user.id, 'Вы успешно вышли с аккаунта',
-                                       reply_markup=StartKeyboard)
-                await state.finish()
+@dp.message_handler(state= ClientStatesGroup.LoginEduTatar)
+async def login_edutatar(message: types.Message, state: FSMContext):
+    user_info = message.text.split("\n")
+    #Проверка сообщения на наличии две строки
+    if len(user_info) == 2:
         
+        #Получаем Логин и Пароль для входа на сайт от пользователя
+        loginFromMessage = message.text.split("\n")[0]
+        passwordFromMessage = message.text.split("\n")[1]
+
+        #Импорт функции парсера
+        sys.path.insert(0, "C:/Users/user/MyProjects/MyPythonProjects/Edu Tatar/script")
+        import script_login
+
+        #Получаем ответ от парсера
+        responce = script_login.loginEduTatar(loginFromMessage, passwordFromMessage)
+
+        #Проверка ответа на true и false
+        if responce[0] == 'true':
+
+            #Вывод сообщения об успешной авторизации и данными аккаунта
+            await bot.send_message(message.from_user.id, f"Успешная авторизация!\nЛогин: {responce[1][0]}\nФИО: {responce[1][0]}")
+
+            #Массив с оценками от парсера
+            subjects = responce[4]
+
+            #Удаляем ненужными массивы в массиве
+            subjects.pop(len(subjects) - 1)
+            subjects.pop(len(subjects) - 1)
+
+            #Выводим оценки пользователя таким образом: <Имя предмета>: <Оценки>, <Средний балл>
+            for items in subjects:
+                grades = ' '.join(items[1:-1])
+                await bot.send_message(message.from_user.id, f'{items[0]}: {grades}, {items[-1]}')
+            await bot.send_message(message.from_user.id, "Все оценки были выведены\nВыполнен автоматический выход с аккаунта!")
+            await state.finish()
+            
+        elif responce[0] == 'false':
+            await bot.send_message(message.from_user.id, "Упс! Не удалось выполнить вход по указанным данным.\nПроверьте правильность написания Логина и Пароля")
+            await state.finish()
+
+    elif len(user_info) != 2:
+        await bot.send_message(message.from_user.id, "Упс! Не удалось выполнить вход по указанным данным.\nПроверьте правильность написания Логина и Пароля")
+        await state.finish()
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
--- script/script_login.py
+++ script/script_login.py
@@ -1,88 +1,88 @@
 import requests
 from bs4 import BeautifulSoup
 
+import requests
+from bs4 import BeautifulSoup
+
 def loginEduTatar(LoginFromMessage, PasswordFromMessage):
-    #Вход на сайт Edu Tatar
-    UrlLogin_EduTatar = "https://edu.tatar.ru/logon"
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-    session = requests.Session()
-    responce = session.get(UrlLogin_EduTatar, headers={
-        'User-Agent' : user_agent
-    })
-    session.headers.update({'Referer':UrlLogin_EduTatar})
-    session.headers.update({'User-Agent':user_agent})
+    #Срабатывает исключение при неправильных данных для входа от пользователя
+    try:
+        #Вход на сайт Edu Tatar
+        UrlLogin_EduTatar = "https://edu.tatar.ru/logon"
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        session = requests.Session()
+        responce = session.get(UrlLogin_EduTatar, headers={
+            'User-Agent' : user_agent
+        })
+        session.headers.update({'Referer':UrlLogin_EduTatar})
+        session.headers.update({'User-Agent':user_agent})
 
-    #Получаем данные для входа от пользователя
-    data = {
-        'main_login2' : LoginFromMessage,
-        'main_password2' : PasswordFromMessage
-    }
-    #Делаем пост запрос на сайт
-    post_requests = session.post(UrlLogin_EduTatar,
-                                 data)
+        #Получаем данные для входа от пользователя
+        data = {
+            'main_login2' : LoginFromMessage,
+            'main_password2' : PasswordFromMessage
+        }
+        #Делаем пост запрос на сайт
+        post_requests = session.post(UrlLogin_EduTatar,
+                                     data)
 
-    #Сработает исключение при неправильных данных
-    
-        #Получаем информацию(Имя, логин, должность) с сайта после успешной авторизации
-    BS_User_Anceta = BeautifulSoup(post_requests.text, "lxml")
-    UserInfoFromAnceta = BS_User_Anceta.find_all('td')
-    UserNameFromAnceta = UserInfoFromAnceta[1].text
-    UserLoginFromAnceta = UserInfoFromAnceta[3].text
-    UserJobFromAnceta = UserInfoFromAnceta[5].text
-    UserInfoList_FromAnceta = [UserNameFromAnceta, UserLoginFromAnceta, UserJobFromAnceta]
+        #Сработает исключение при неправильных данных
 
+            #Получаем информацию(Имя, логин, должность) с сайта после успешной авторизации
+        BS_User_Anceta = BeautifulSoup(post_requests.text, "lxml")
+        UserInfoFromAnceta = BS_User_Anceta.find_all('td')
+        UserNameFromAnceta = UserInfoFromAnceta[1].text
+        UserLoginFromAnceta = UserInfoFromAnceta[3].text
+        UserJobFromAnceta = UserInfoFromAnceta[5].text
+        UserInfoList_FromAnceta = [UserNameFromAnceta, UserLoginFromAnceta, UserJobFromAnceta]
 
+        Url_UserDiaryTerm = "https://edu.tatar.ru/user/diary/term"
+        responce_diaryTerm = session.get(url=Url_UserDiaryTerm)
+
+        BS_User_Diary_Term = BeautifulSoup(responce_diaryTerm.text, 'lxml')
+        test = BS_User_Diary_Term.find_all('tr')
+
+        #обрабатываю html страницу    
+        contrainer = BS_User_Diary_Term.find_all("tr")
+        strOut = ""
+        data_list = []
+        for elem in contrainer:
+            key = elem.text
+            data_list.append(key)
+
+        #не обработанные оценки и предметы в списке
+        DataSubjectsList = data_list
+        DataSubjectsList.pop(0)
+        SubjectsAndRatings = []
+        test1 = []
+        items = []
+
+        #обработка
+        for subject in DataSubjectsList:
+            a = subject.split('\n')
+            if subject != '':
+                items.append(a)
+
+        #Добавляем название предмета и оценки [[name, ''] [name, '']]
+        for subject in items:
+            SubjectsAndRatings.append([subject[1]])
+            nameSubject = subject[1]
+            for rating in subject:
+                if len(rating) == 1 or len(rating) == 4:
+                    for i in SubjectsAndRatings:
+                        for name in i:
+                            if nameSubject == name:
+                                i.append(rating)
+
+        #Добавляем пункт ИТОГО в список
+        generalAllSubjectList = len(SubjectsAndRatings) - 1
+        generalAllSubject = items[generalAllSubjectList][0].split('ИТОГО')[1]
+        SubjectsAndRatings.append([generalAllSubject])
+
+        session.get(url="https://edu.tatar.ru/logoff")
 
         
-    return ['true', UserInfoList_FromAnceta, session, user_agent,  LoginFromMessage, PasswordFromMessage]
-
-def diary(LoginFromMessage, PasswordFromMessage):
-    LoginEduTatar = loginEduTatar(LoginFromMessage, PasswordFromMessage)
-    session = LoginEduTatar[2]
-    Url_UserDiaryTerm = "https://edu.tatar.ru/user/diary/term"
-    responce_diaryTerm = session.get(url=Url_UserDiaryTerm)
-
-    BS_User_Diary_Term = BeautifulSoup(responce_diaryTerm.text, 'lxml')
-
-    #обрабатываю html страницу    
-    contrainer = BS_User_Diary_Term.find_all("tr")
-    strOut = ""
-    data_list = []
-    for elem in contrainer:
-        key = elem.text
-        data_list.append(key)
-
-    #не обработанные оценки и предметы в списке
-    DataSubjectsList = data_list
-    DataSubjectsList.pop(0)
-    SubjectsAndRatings_List = []
-    SubjectsAndRatings_Dict = {}
-    Ratings = []
-    test1 = []
-    items = []
+        return ['true', UserInfoList_FromAnceta, session, user_agent, SubjectsAndRatings, LoginFromMessage, PasswordFromMessage]
     
-    #обработка
-    for subject in DataSubjectsList:
-        a = subject.split('\n')
-        if subject != '':
-            items.append(a)
-    
-    #Добавляем название предмета и оценки [[name, ''] [name, '']]
-    for subject in items:
-        SubjectsAndRatings_List.append([subject[1]])
-        nameSubject = subject[1]
-        for rating in subject:
-            if len(rating) == 1 or len(rating) == 4:
-                for i in SubjectsAndRatings_List:
-                    for name in i:
-                        if nameSubject == name:
-                            i.append(rating)
-
-    generalAllSubjectList = len(SubjectsAndRatings_List) - 1
-    generalAllSubject = items[generalAllSubjectList][0].split('ИТОГО')[1]
-    SubjectsAndRatings_List.append([generalAllSubject])
-
-    r = requests.post(url=Url_UserDiaryTerm, headers={'Connection':'close'})
-
-    return [SubjectsAndRatings_List]
-
+    except:
+        return['false' , LoginFromMessage, PasswordFromMessage]
